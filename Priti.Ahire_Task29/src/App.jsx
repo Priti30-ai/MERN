3dcd8535cbb89923ef8e00fd053ea24800d03160
Priti30-ai <ahirespriti@gmail.com>
2026-09-25 00:01:04 +0530

    Fix Task 29 payment deployment

--- Priti.Ahire_Task29/src/App.jsx
+++ Priti.Ahire_Task29/src/App.jsx
@@ -1,4 +1,5 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Payment from "./pages/Payment";
@@ -6,6 +7,8 @@ import Payment from "./pages/Payment";
 function App() {
   return (
     <HashRouter>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
