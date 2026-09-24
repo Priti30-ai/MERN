66136488ffec37b63590a72ef6a8aa19ce0fd74e
Priti30-ai <ahirespriti@gmail.com>
2026-09-24 23:15:16 +0530

    Add Task 29 payment functionality

--- Priti.Ahire_Task29/src/App.jsx
+++ Priti.Ahire_Task29/src/App.jsx
@@ -1,22 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Cart from './pages/Cart'
-import Payment from './pages/Payment'
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Payment from "./pages/Payment";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/payment" element={<Payment />} />
       </Routes>
-    </BrowserRouter>
-  )
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
